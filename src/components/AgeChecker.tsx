f0522f5b69ca12f42025e640a37220c18de0fd66
Richard Miller <richardmmill@gmail.com>
2021-07-26 17:03:47 -0400

    age checker

--- src/components/AgeChecker.tsx
+++ src/components/AgeChecker.tsx
@@ -1,14 +1,23 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 const AgeChecker: React.FC = () => {
     const [age, setAge] = useState<boolean>(false);
 
+    const dispatch = useDispatch();
+
+    const approveAge = () => {
+        dispatch({type: "OLD_ENOUGH", payload: age})
+    }
+
     return (
-        <div>
+        <section>
             <h3>Are 21 years or older?</h3>
-            <button>Yes, I am 21 or over</button>
-            <button>no</button>
-        </div>
+            <div>
+                <button onClick={() => setAge(true)}>Yes, I am 21 or over</button>
+                <button>no</button>
+            </div>
+        </section>
     )
 }
 
